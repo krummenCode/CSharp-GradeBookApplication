69d06d4c0399c3747f5afb0168c95446593c4c43
DJ Krummen <44108933+krummenCode@users.noreply.github.com>
2020-01-03 10:12:20 -0600

    + Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,7 +13,9 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
+            {
                 throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
@@ -38,8 +40,6 @@ namespace GradeBook.GradeBooks
             {
                 return 'F';
             }
-
-            return base.GetLetterGrade(averageGrade);
         }
     }
 }
